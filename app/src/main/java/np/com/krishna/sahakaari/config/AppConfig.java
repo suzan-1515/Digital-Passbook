bac1d784ee0d891e2810d71f02f2b4c883599be9
Sujan Parajuli <suzanparajuli@gmail.com>
2019-05-27 16:12:05 +0545

    api updated

--- app/src/main/java/np/com/krishna/sahakaari/config/AppConfig.java
+++ app/src/main/java/np/com/krishna/sahakaari/config/AppConfig.java
@@ -2,6 +2,6 @@ package np.com.krishna.sahakaari.config;
 
 public class AppConfig {
 
-    public static final String BASE_URL = "https://16bb72fb.ngrok.io/api/v1/passbook/";
+    public static final String BASE_URL = "https://digitalpassbook.herokuapp.com/api/v1/passbook/";
 
 }
